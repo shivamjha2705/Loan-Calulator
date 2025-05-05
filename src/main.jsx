72d7d9acb7dcae8c48c4054bfbcca17dd2f1f0ac
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-05-05 23:01:32 +0530

    updated main to wrapup with app provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { AppProvider } from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+   <BrowserRouter>
+   <AppProvider>
     <App />
+    </AppProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
