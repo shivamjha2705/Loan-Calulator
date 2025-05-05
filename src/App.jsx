bfc73240ca3dc59c7429c3b296a524ee660aac10
SHIVAM KUMAR <shivamjha2705@gmail.com>
2025-05-05 23:00:43 +0530

    updated app with context api and theme provider with global header

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,29 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import ExchangeRates from './pages/ExchangeRates'
 import About from './pages/About'
-import NotFound from './pages/NotFound'
+import NotFound from './pages/PageNotFound'
+import { getTheme } from './theme'
+import { useAppContext } from './context/AppContext'
+import Header from './components/Header'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
 
 const App = () => {
+  const { themeMode } = useAppContext();
   return (
+    <ThemeProvider theme={getTheme(themeMode)}>
+      <CssBaseline />
+
+      {/* ----Global Navigation Bar---- */}
+      <Header /> 
+
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/exchange-rates" element={<ExchangeRates />} />
       <Route path="/about" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
+    </ThemeProvider>
   )
 }
 
